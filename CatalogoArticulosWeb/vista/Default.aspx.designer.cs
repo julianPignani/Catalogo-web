354cf4ccbf1f40a262e5225eb7daf345c1bfe328
Julian Pignani <pignanijulian@gmail.com>
2024-01-07 10:19:02 -0300

    Agregue un UpdatePanel al filtro del Default.aspx

--- CatalogoArticulosWeb/vista/Default.aspx.designer.cs
+++ CatalogoArticulosWeb/vista/Default.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace vista
     {
 
         /// <summary>
+        /// ScripManager1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.ScriptManager ScripManager1;
+
+        /// <summary>
         /// lblCampo control.
         /// </summary>
         /// <remarks>
